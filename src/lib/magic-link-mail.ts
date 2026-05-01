d5fec9d8c5ad30be9b05f26d783e8bd9877e0812
illionair <133358435+illionair@users.noreply.github.com>
2026-05-01 16:26:24 +0200

    Prefer public site URL for magic links

--- src/lib/magic-link-mail.ts
+++ src/lib/magic-link-mail.ts
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 function siteUrl() {
     if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "");
-    if (process.env.AUTH_URL) return process.env.AUTH_URL.replace(/\/$/, "");
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    if (process.env.AUTH_URL) return process.env.AUTH_URL.replace(/\/$/, "");
     return "http://localhost:3000";
 }
 
